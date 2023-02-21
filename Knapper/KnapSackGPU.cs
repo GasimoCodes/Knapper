a8437b85f5e86a88e1212bf3a39767074a886846
Gasimo <prokop.svacina@gmail.com>
2023-02-22 00:09:41 +0100

    Started adding GPU solver

---
+++ Knapper/KnapSackGPU.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ILGPU;
+using ILGPU.Runtime;
+using ILGPU.Runtime.CPU;
+using System.Threading;
+using System.Numerics;
+using ILGPU.Runtime.OpenCL;
+using ILGPU.IR;
+using System.Diagnostics;
+using ILGPU.Algorithms;
+using ILGPU.Backends;
+
+namespace Knapper
+{
+    public static class KnapSackGPU
+    {
+
+
+        public static void KnapsackSolver(Vector2[] list, int capacity)
+        {
+            using Context context = Context.Create(builder => builder.AllAccelerators());
+            Device d = context.GetPreferredDevice(preferCPU: false);
+            Accelerator acc = d.CreateAccelerator(context);
+
+            int n = list.Length;
+            ulong combinationCount = (1UL << n) - 1;
+            
+            // Allocate and copy the init list
+            MemoryBuffer1D<Vector2, Stride1D.Dense> inputs = acc.Allocate1D(list);
+            // Allocate Vector for each thread
+            MemoryBuffer1D<Vector3, Stride1D.Dense> results = acc.Allocate1D<Vector3>(acc.MaxNumThreads);
+            MemoryBuffer1D<Vector3, Stride1D.Dense> staticMemory = acc.Allocate1D<Vector3>(0);
+
+
+            Console.WriteLine($"DEV: {acc.Name}");
+            Console.WriteLine($"\nTOTAL COMBINATIONS: {combinationCount}");
+            Console.WriteLine("AVAILABLE THREADS: " + acc.MaxNumThreads);
+
+            int iterCount = (int)((combinationCount + (ulong)acc.MaxNumThreads - 1UL) / (ulong)acc.MaxNumThreads);
+            int chunkSize = acc.MaxNumThreads;
+            Console.WriteLine("App will run for " + iterCount + " iteration(s)\n\n");
+            
+            
+            // Upload kernel
+            Action<Index1D, ArrayView<Vector2>, ArrayView<Vector3>, int, ArrayView<Vector3>> loadedKernel =
+    acc.LoadAutoGroupedStreamKernel<Index1D, ArrayView<Vector2>, ArrayView<Vector3>, int, ArrayView<Vector3>>(Kernel);
+
+
+            Vector2 bestResult = new Vector2();
+            bool[] currentBestBits = new bool[n];
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+
+            // Repeat this until we went over ALL the elements
+            for (int t = 0; t < iterCount; t++)
+            {
+                // What will we cover in this iteration
+                int start = t * chunkSize;
+                int end = (int)Math.Min(((ulong)t + 1UL) * (ulong)chunkSize, combinationCount);
+
+                // Console.WriteLine("START: " + start + "END " + end);
+
+                loadedKernel(end-start, inputs.View, results.View, start, staticMemory.View);
+                acc.Synchronize();
+
+
+                // Get best value, somehow
+
+
+                // Store best value
+
+
+            }
+
+            sw.Stop();
+
+
+            /*
+            foreach (Vector2 output in results.GetAsArray1D())
+            {
+                Console.WriteLine($"X: {output.X}, Y: {output.Y}");
+            }*/
+
+            Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+
+            acc.Dispose();
+            context.Dispose();
+
+
+        }
+
+
+        static void Kernel(Index1D index, ArrayView<Vector2> data, ArrayView<Vector3> output, int indexOffset, ArrayView<Vector3> bestValue)
+        {
+
+            float value = 0;
+            float weight = 0;
+            int combinationIndex = (index+1)+ indexOffset;
+
+
+            for (int elementIndex = 0; elementIndex < data.Length; elementIndex++)
+            {
+                if (((combinationIndex) & (1 << elementIndex)) != 0)
+                {
+                    value += data[elementIndex].X;
+                    weight += data[elementIndex].Y;
+                    // currentBits[elementIndex] = true;
+                }
+                else
+                {
+                    // currentBits[elementIndex] = false;
+                }
+            }
+
+            /*
+            // If the current combination is better than the last best
+            if (weight <= capacity && value > bestResult.Y)
+            {
+                bestResult = new Vector2(weight, value);
+                currentBestBits = currentBits;
+            }
+            */
+
+            output[index] = new Vector3(weight, value, (combinationIndex-1));
+        }
+
+
+        static void KernelBestValue(Index1D index, ArrayView<Vector2> data, ArrayView<Vector3> bestValue)
+        {
+
+            
+            /*
+            // If the current combination is better than the last best
+            if (weight <= capacity && value > bestResult.Y)
+            {
+                bestResult = new Vector2(weight, value);
+                currentBestBits = currentBits;
+            }
+            */
+
+        }
+
+
+
+
+    }
+}
--- Knapper/Program.cs
+++ Knapper/Program.cs
@@ -2,190 +2,196 @@
 using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using ILGPU.Runtime;
+using ILGPU.Runtime.CPU;
+using ILGPU;
 
 namespace Knapper
 {
 
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			Stopwatch stopwatch = Stopwatch.StartNew();
-			int maxCapacity = 65;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxCapacity = 60;
 
+            // Weight, value
+            Vector2[] items = {
+                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
+                              new Vector2(1, 10),
+                new Vector2(2, 4),
+                                new Vector2(1, 10),
+                new Vector2(2, 4),
 
-			Vector2[] items = {
-				new Vector2(1, 10),
-				new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(1, 10),
-				new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(1, 10),
-				new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(1, 10),
-				new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(1, 10),
-				new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(2, 4),
-				new Vector2(4, 12),
-								new Vector2(2, 4),
-				new Vector2(4, 12),
+            };
 
-			};
+            KnapSackGPU.KnapsackSolver(items, maxCapacity);
+            
 
-			Vector2[][] result;
-
-			
-			stopwatch.Restart();
-			result = GetCombinationOptimized(items);
-			Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
-			
-			//Console.ReadLine();
-			printCombinations(result);
-
-
-
-			stopwatch.Restart();
-			result = GetCombinationOptimizedThreaded(items);
-			Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
-
-			//Console.ReadLine();
-			printCombinations(result);
-
-			/*
-			stopwatch.Restart();
-			result = GetCombinationOptimizedParallel(items);
-			Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
-			
-			//Console.ReadLine();
-			printCombinations(result);
-			
-			*/
-
-			
-
-			// Vector2[] value = getBestCapacity(GetCombinationOptimizedParallel(items), 3);
-
-		}
-
-
-
-		public static Vector2[][] GetCombinationOptimized(Vector2[] list)
-		{
-			int n = list.Length;
-			int count = (1 << n) - 1;
-
-			Vector2[][] results = new Vector2[count][];
-
-			// For each combination
-			for (int combinationIndex = 1; combinationIndex <= count; combinationIndex++)
-			{
-				List<Vector2> combination = new List<Vector2>();
-
-				// For each element in list
-				for (int elementIndex = 0; elementIndex < n; elementIndex++)
-				{
-					// Check whether to include the item in the combination using bit shift
-					if ((combinationIndex & (1 << elementIndex)) != 0)
-					{
-						//Console.Write(list[elementIndex].X);
-						combination.Add(list[elementIndex]);
-					}
-				}
-
-				results[combinationIndex - 1] = combination.ToArray();
-				//Console.WriteLine();
-			}
-
-			return results;
-		}
-
-		public static Vector2[] getBestCapacity(Vector2[][] input, int maxCapacity)
-		{
-			// Index same as input index, (int1 = value, int2 = weight)
-			Dictionary<KeyValuePair<int, int>, Vector2[]> values = new Dictionary<KeyValuePair<int, int>, Vector2[]>();
-
-			foreach (Vector2[] val in input)
-			{
-				int value = (int)val.Sum(x => x.Y);
-				int weight = (int)val.Sum(x => x.X);
-
-				//				KeyValuePair<int, int> keyPair = new KeyValuePair<int, int> ( value, weight );
-
-				values.TryAdd(new KeyValuePair<int, int>(value, weight), val);
-			}
-
-			foreach (KeyValuePair<KeyValuePair<int, int>, Vector2[]> val in values.OrderByDescending(x => x.Key.Key))
-			{
-				if (val.Key.Value <= maxCapacity)
-				{
-					Console.WriteLine($"VALUE: {val.Key.Key} WEIGHT: {val.Key.Value}");
-					return val.Value;
-				}
-			}
-
-			return null;
-		}
-
-		public static Vector2[][] GetCombinationOptimizedParallel(Vector2[] list)
-		{
-			int n = list.Length;
-			int count = (1 << n) - 1;
-			int maxDegreeOfParallelism = Environment.ProcessorCount;
-
-			Vector2[][] results = new Vector2[count][];
-
-			int chunkSize = count / maxDegreeOfParallelism;
-
-			List<Task> tasks = new List<Task>();
-			for (int i = 0; i < maxDegreeOfParallelism; i++)
-			{
-				int startIndex = i * chunkSize + 1;
-				int endIndex = (i == maxDegreeOfParallelism - 1) ? count : (i + 1) * chunkSize;
-
-				tasks.Add(Task.Run(() =>
-				{
-					for (int combinationIndex = startIndex; combinationIndex <= endIndex; combinationIndex++)
-					{
-						List<Vector2> combination = new List<Vector2>();
-
-						for (int elementIndex = 0; elementIndex < n; elementIndex++)
-						{
-							if ((combinationIndex & (1 << elementIndex)) != 0)
-							{
-								combination.Add(list[elementIndex]);
-							}
-						}
-
-						results[combinationIndex - 1] = combination.ToArray();
-					}
-				}));
-			}
-
-			Task.WaitAll(tasks.ToArray());
-
-			return results;
-		}
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Vector2[][] result;
+            
+            Console.WriteLine("Total items: " + items.Length);
+            
+            /*
+            stopwatch.Restart();
+            KnapsackSolverThreaded(items, maxCapacity);
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
+            */
+            
+        }
 
 
 
 
 
+        public static Vector2[] KnapsackSolver(Vector2[] list, int capacity)
+        {
+            list = list.OrderByDescending(x => x.Y).ToArray();
 
-		public static void printCombinations(Vector2[][] list)
-		{
-			string toWrite = "";
-			/*
+            int n = list.Length;
+            int count = (1 << n) - 1;
+
+            // Combination index, Item index
+            bool[][] results = new bool[count][];
+            // Combination index, weight, value
+            Vector2[] resultValues = new Vector2[count];
+
+            int bestResultIndex = 0;
+
+            // For each combination
+            for (int combinationIndex = 1; combinationIndex <= count; combinationIndex++)
+            {
+
+                // For each element in list
+                for (int elementIndex = 0; elementIndex < n; elementIndex++)
+                {
+                    // Check whether to include the item in the combination using bit shift
+                    if ((combinationIndex & (1 << elementIndex)) != 0)
+                    {
+                        //results[combinationIndex - 1][elementIndex-1] = true;
+                        resultValues[combinationIndex - 1].X += list[elementIndex].X;
+                        resultValues[combinationIndex - 1].Y += list[elementIndex].Y;
+                    }
+                }
+
+                // If the current combination is better than the last best
+                if (resultValues[combinationIndex - 1].X <= capacity && resultValues[combinationIndex - 1].Y > resultValues[bestResultIndex].Y)
+                    bestResultIndex = (combinationIndex - 1);
+
+            }
+
+            Console.WriteLine("BEST WEIGHT: " + resultValues[bestResultIndex].X + " BEST VALUE: " + resultValues[bestResultIndex].Y);
+
+            return null;
+        }
+
+
+
+
+
+        private static bool[] KnapsackSolverThreaded(Vector2[] list, int capacity)
+        {
+
+            list = list.OrderByDescending(x => x.Y).ToArray();
+
+            int n = list.Length;
+            int combinationCount = (1 << n) - 1;
+
+            int numThreads = Environment.ProcessorCount;
+            var tasks = new Task[numThreads];
+            int chunkSize = (combinationCount + numThreads - 1) / numThreads;
+
+
+            Vector2 bestResult = new Vector2();
+            bool[] currentBestBits = new bool[n];
+
+            for (int t = 0; t < numThreads; t++)
+            {
+                // Combination index, Item index
+
+                int start = t * chunkSize + 1;
+                int end = Math.Min((t + 1) * chunkSize, combinationCount);
+
+                tasks[t] = Task.Factory.StartNew(() =>
+                {
+                    float value = 0;
+                    float weight = 0;
+                    bool[] currentBits = new bool[n];
+
+                    for (int combinationIndex = start; combinationIndex <= end; combinationIndex++)
+                    {
+                        value = 0;
+                        weight = 0;
+
+                        for (int elementIndex = 0; elementIndex < n; elementIndex++)
+                        {
+                            if ((combinationIndex & (1 << elementIndex)) != 0)
+                            {
+                                value += list[elementIndex].X;
+                                weight += list[elementIndex].Y;
+                                currentBits[elementIndex] = true;
+                            }
+                            else
+                            {
+                                currentBits[elementIndex] = false;
+                            }
+                        }
+
+
+                        // If the current combination is better than the last best
+                        if (weight <= capacity && value > bestResult.Y)
+                        {
+                            bestResult = new Vector2(weight, value);
+                            currentBestBits = currentBits;
+                        }
+                    }
+                });
+            }
+            Task.WaitAll(tasks.Where(t => t != null).ToArray());
+
+            Console.WriteLine("BEST WEIGHT: " + bestResult.X + " BEST VALUE: " + bestResult.Y);
+
+            return currentBestBits;
+
+        }
+
+        public static void printCombinations(Vector2[][] list)
+        {
+            string toWrite = "";
+            /*
 			foreach (Vector2[] innerList in list)
 			{
 				foreach (Vector2 inner in innerList)
@@ -195,60 +201,11 @@ namespace Knapper
 				toWrite += "\n";
 			}
 			*/
-			toWrite += ("---\n TOTAL OF: " + list.Length + "\n---");
+            toWrite += ("---\n TOTAL OF: " + list.Length + "\n---");
 
-			Console.WriteLine(toWrite);
+            Console.WriteLine(toWrite);
 
-		}
+        }
 
-
-		private static Vector2[][] GetCombinationOptimizedThreaded(Vector2[] list)
-		{
-
-			int n = list.Length;
-			int count = (1 << n) - 1;
-			int numThreads = Environment.ProcessorCount;
-			var tasks = new Task[numThreads];
-			int chunkSize = (count + numThreads - 1) / numThreads;
-
-			Vector2[][] results = new Vector2[count][];
-
-
-			for (int t = 0; t < numThreads; t++)
-			{
-
-				int start = t * chunkSize + 1;
-				int end = Math.Min((t + 1) * chunkSize, count);
-
-				tasks[t] = Task.Factory.StartNew(() =>
-				{
-					for (int i = start; i <= end; i++)
-					{
-
-						List<Vector2> combination = new List<Vector2>();
-
-						for (int j = 0; j < n; j++)
-						{
-							if ((i & (1 << j)) != 0)
-							{
-								// Console.Write(list[j].X);
-
-								combination.Add(list[j]);
-							}
-						}
-
-						results[i - 1] = combination.ToArray();
-
-
-					}
-				});
-
-			}
-			Task.WaitAll(tasks.Where(t => t != null).ToArray());
-
-			return results;
-
-		}
-
-	}
+    }
 }
