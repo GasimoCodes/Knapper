acd6e934e1cda5c90b6e58f647f4c34e59937f78
Gasimo <prokop.svacina@gmail.com>
2023-02-23 11:20:13 +0100

    Added greedy approximation solver

---
+++ Knapper/KnapSackHeuristics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Knapper
+{
+	internal class KnapSackHeuristics
+	{
+
+        public static Vector2[] KnapsackSolver(Vector2[] list, int capacity)
+        {
+            // sort the items in decreasing order of value per unit weight
+            Array.Sort(list, (a, b) => (b.Y / b.X).CompareTo(a.Y / a.X));
+
+            // initialize the knapsack
+            int n = list.Length;
+            Vector2[] knapsack = new Vector2[n];
+
+            // add items to the knapsack until it is full
+            int i = 1;
+            while (capacity > 0 && i < n)
+            {
+                int weight = (int)list[i].X;
+                knapsack[i] = list[i];
+                capacity -= weight;
+                i++;
+            }
+
+            Console.WriteLine("Approximation: Weight: " + knapsack.Sum(x => x.X) + " Value: " + knapsack.Sum(x => x.Y));
+
+            return knapsack;
+        }
+
+
+    }
+}
--- Knapper/Program.cs
+++ Knapper/Program.cs
@@ -12,15 +12,19 @@ using ILGPU;
 namespace Knapper
 {
 
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int maxCapacity = 60;
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int maxCapacity = 3;
 
-            // Weight, value
-            Vector2[] items = {
-                new Vector2(1, 10),
+			// Weight, value
+			Vector2[] items = {
+				new Vector2(1, 10),
+				new Vector2(2, 4),
+				new Vector2(3, 15),
+				new Vector2(3, 15),
+                
                 new Vector2(2, 4),
                               new Vector2(1, 10),
                 new Vector2(2, 4),
@@ -36,6 +40,7 @@ namespace Knapper
                 new Vector2(2, 4),
                               new Vector2(1, 10),
                 new Vector2(2, 4),
+				/*
                                 new Vector2(1, 10),
                 new Vector2(2, 4),
                               new Vector2(1, 10),
@@ -45,153 +50,159 @@ namespace Knapper
                               new Vector2(1, 10),
                 new Vector2(2, 4),
                                 new Vector2(1, 10),
-                new Vector2(2, 4),
-                              new Vector2(1, 10),
-                new Vector2(2, 4),
-                                new Vector2(1, 10),
-                new Vector2(2, 4),
-                              new Vector2(1, 10),
-                new Vector2(2, 4),
-                                new Vector2(1, 10),
-                new Vector2(2, 4),
+                new Vector2(2, 4),*/
 
             };
 
-            KnapSackGPU.KnapsackSolver(items, maxCapacity);
-            
 
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            Vector2[][] result;
-            
-            Console.WriteLine("Total items: " + items.Length);
-            
-            /*
+			// GPU
+			// KnapSackGPU.KnapsackSolver(items, maxCapacity);
+
+			
+			// Approx.
+			KnapSackHeuristics.KnapsackSolver(items, maxCapacity);
+
+			// CPU
+			KnapsackSolver(items, maxCapacity);
+
+			KnapsackSolverThreaded(items, maxCapacity);
+
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			Vector2[][] result;
+
+
+
+			Console.WriteLine("Total items: " + items.Length);
+
+
+
+			/*
             stopwatch.Restart();
-            KnapsackSolverThreaded(items, maxCapacity);
+            
             Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
             */
-            
-        }
+
+		}
 
 
 
 
 
-        public static Vector2[] KnapsackSolver(Vector2[] list, int capacity)
-        {
-            list = list.OrderByDescending(x => x.Y).ToArray();
+		public static Vector2[] KnapsackSolver(Vector2[] list, int capacity)
+		{
+			list = list.OrderByDescending(x => x.Y).ToArray();
 
-            int n = list.Length;
-            int count = (1 << n) - 1;
+			int n = list.Length;
+			int count = (1 << n) - 1;
 
-            // Combination index, Item index
-            bool[][] results = new bool[count][];
-            // Combination index, weight, value
-            Vector2[] resultValues = new Vector2[count];
+			// Combination index, Item index
+			bool[][] results = new bool[count][];
+			// Combination index, weight, value
+			Vector2[] resultValues = new Vector2[count];
 
-            int bestResultIndex = 0;
+			int bestResultIndex = 0;
 
-            // For each combination
-            for (int combinationIndex = 1; combinationIndex <= count; combinationIndex++)
-            {
+			// For each combination
+			for (int combinationIndex = 1; combinationIndex <= count; combinationIndex++)
+			{
 
-                // For each element in list
-                for (int elementIndex = 0; elementIndex < n; elementIndex++)
-                {
-                    // Check whether to include the item in the combination using bit shift
-                    if ((combinationIndex & (1 << elementIndex)) != 0)
-                    {
-                        //results[combinationIndex - 1][elementIndex-1] = true;
-                        resultValues[combinationIndex - 1].X += list[elementIndex].X;
-                        resultValues[combinationIndex - 1].Y += list[elementIndex].Y;
-                    }
-                }
+				// For each element in list
+				for (int elementIndex = 0; elementIndex < n; elementIndex++)
+				{
+					// Check whether to include the item in the combination using bit shift
+					if ((combinationIndex & (1 << elementIndex)) != 0)
+					{
+						//results[combinationIndex - 1][elementIndex-1] = true;
+						resultValues[combinationIndex - 1].X += list[elementIndex].X;
+						resultValues[combinationIndex - 1].Y += list[elementIndex].Y;
+					}
+				}
 
-                // If the current combination is better than the last best
-                if (resultValues[combinationIndex - 1].X <= capacity && resultValues[combinationIndex - 1].Y > resultValues[bestResultIndex].Y)
-                    bestResultIndex = (combinationIndex - 1);
+				// If the current combination is better than the last best
+				if (resultValues[combinationIndex - 1].X <= capacity && resultValues[combinationIndex - 1].Y > resultValues[bestResultIndex].Y)
+					bestResultIndex = (combinationIndex - 1);
 
-            }
+			}
 
-            Console.WriteLine("BEST WEIGHT: " + resultValues[bestResultIndex].X + " BEST VALUE: " + resultValues[bestResultIndex].Y);
+			Console.WriteLine("Compute: Weight: " + resultValues[bestResultIndex].X + " Value: " + resultValues[bestResultIndex].Y);
 
-            return null;
-        }
-
-
+			return null;
+		}
 
 
 
-        private static bool[] KnapsackSolverThreaded(Vector2[] list, int capacity)
-        {
-
-            list = list.OrderByDescending(x => x.Y).ToArray();
-
-            int n = list.Length;
-            int combinationCount = (1 << n) - 1;
-
-            int numThreads = Environment.ProcessorCount;
-            var tasks = new Task[numThreads];
-            int chunkSize = (combinationCount + numThreads - 1) / numThreads;
 
 
-            Vector2 bestResult = new Vector2();
-            bool[] currentBestBits = new bool[n];
+		private static bool[] KnapsackSolverThreaded(Vector2[] list, int capacity)
+		{
 
-            for (int t = 0; t < numThreads; t++)
-            {
-                // Combination index, Item index
+			list = list.OrderByDescending(x => x.Y).ToArray();
 
-                int start = t * chunkSize + 1;
-                int end = Math.Min((t + 1) * chunkSize, combinationCount);
+			int n = list.Length;
+			int combinationCount = (1 << n) - 1;
 
-                tasks[t] = Task.Factory.StartNew(() =>
-                {
-                    float value = 0;
-                    float weight = 0;
-                    bool[] currentBits = new bool[n];
-
-                    for (int combinationIndex = start; combinationIndex <= end; combinationIndex++)
-                    {
-                        value = 0;
-                        weight = 0;
-
-                        for (int elementIndex = 0; elementIndex < n; elementIndex++)
-                        {
-                            if ((combinationIndex & (1 << elementIndex)) != 0)
-                            {
-                                value += list[elementIndex].X;
-                                weight += list[elementIndex].Y;
-                                currentBits[elementIndex] = true;
-                            }
-                            else
-                            {
-                                currentBits[elementIndex] = false;
-                            }
-                        }
+			int numThreads = Environment.ProcessorCount;
+			var tasks = new Task[numThreads];
+			int chunkSize = (combinationCount + numThreads - 1) / numThreads;
 
 
-                        // If the current combination is better than the last best
-                        if (weight <= capacity && value > bestResult.Y)
-                        {
-                            bestResult = new Vector2(weight, value);
-                            currentBestBits = currentBits;
-                        }
-                    }
-                });
-            }
-            Task.WaitAll(tasks.Where(t => t != null).ToArray());
+			Vector2 bestResult = new Vector2();
+			bool[] currentBestBits = new bool[n];
 
-            Console.WriteLine("BEST WEIGHT: " + bestResult.X + " BEST VALUE: " + bestResult.Y);
+			for (int t = 0; t < numThreads; t++)
+			{
+				// Combination index, Item index
 
-            return currentBestBits;
+				int start = t * chunkSize + 1;
+				int end = Math.Min((t + 1) * chunkSize, combinationCount);
 
-        }
+				tasks[t] = Task.Factory.StartNew(() =>
+				{
+					float value = 0;
+					float weight = 0;
+					bool[] currentBits = new bool[n];
 
-        public static void printCombinations(Vector2[][] list)
-        {
-            string toWrite = "";
-            /*
+					for (int combinationIndex = start; combinationIndex <= end; combinationIndex++)
+					{
+						value = 0;
+						weight = 0;
+
+						for (int elementIndex = 0; elementIndex < n; elementIndex++)
+						{
+							if ((combinationIndex & (1 << elementIndex)) != 0)
+							{
+								value += list[elementIndex].X;
+								weight += list[elementIndex].Y;
+								currentBits[elementIndex] = true;
+							}
+							else
+							{
+								currentBits[elementIndex] = false;
+							}
+						}
+
+
+						// If the current combination is better than the last best
+						if (weight <= capacity && value > bestResult.Y)
+						{
+							bestResult = new Vector2(weight, value);
+							currentBestBits = currentBits;
+						}
+					}
+				});
+			}
+			Task.WaitAll(tasks.Where(t => t != null).ToArray());
+
+			Console.WriteLine("Threaded: Weight: " + bestResult.X + " Value: " + bestResult.Y);
+
+			return currentBestBits;
+
+		}
+
+		public static void printCombinations(Vector2[][] list)
+		{
+			string toWrite = "";
+			/*
 			foreach (Vector2[] innerList in list)
 			{
 				foreach (Vector2 inner in innerList)
@@ -201,11 +212,11 @@ namespace Knapper
 				toWrite += "\n";
 			}
 			*/
-            toWrite += ("---\n TOTAL OF: " + list.Length + "\n---");
+			toWrite += ("---\n TOTAL OF: " + list.Length + "\n---");
 
-            Console.WriteLine(toWrite);
+			Console.WriteLine(toWrite);
 
-        }
+		}
 
-    }
+	}
 }
